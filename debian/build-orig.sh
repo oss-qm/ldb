477479c08cb513eac613306e8d427c7e208d46ce
ctrlsoft-guest <ctrlsoft-guest>
2007-11-25 18:01:41 +0000

    Add script for building .orig.tar.gz.

---
+++ debian/build-orig.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Build a source tarball for ldb
+
+samba4_repos=svn://svn.samba.org/samba/branches/SAMBA_4_0
+version=$( dpkg-parsechangelog -lldb/debian/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
+
+if echo $version | grep svn > /dev/null; then
+	# SVN Snapshot
+	revno=`echo $version | sed 's/^[0-9.]\+~svn//'`
+	svn export -r$revno $samba4_repos/source/lib/ldb ldb-$version
+	svn export -r$revno $samba4_repos/source/lib/replace ldb-$version/libreplace
+	svn export -r$revno $samba4_repos/source/lib/talloc ldb-$version/libtalloc
+	svn export -r$revno $samba4_repos/source/lib/tdb ldb-$version/libtdb
+else
+	# Release
+	svn export $samba4_repos/../../tags/LDB_`echo $version | sed 's/\./_/g'` ldb-$version
+fi
+
+cd ldb-$version && ./autogen.sh && cd ..
+tar cvz ldb-$version > ldb_$version.orig.tar.gz
