a810ea9ab862ec88efdeba7c690e3a5584e9acee
ctrlsoft-guest <ctrlsoft-guest>
2008-01-22 16:52:11 +0000

    New upstream snapshot, adapt build-orig.sh script to work with git.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -1,20 +1,27 @@
-#!/bin/sh
-# Build a source tarball for ldb
+#!/bin/bash
+GIT_URL=$1
+REFSPEC=$2
+shift 2
 
-samba_repos=svn://svn.samba.org/samba/
-version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
-
-if echo $version | grep svn > /dev/null; then
-	# SVN Snapshot
-	revno=`echo $version | sed 's/^[0-9.]\+~svn//'`
-	svn export -r$revno $samba_repos/branches/SAMBA_4_0/source/lib/ldb ldb-$version
-	svn export -r$revno $samba_repos/branches/SAMBA_4_0/source/lib/replace ldb-$version/libreplace
-	svn export -r$revno $samba_repos/branches/SAMBA_4_0/source/lib/talloc ldb-$version/libtalloc
-	svn export -r$revno $samba_repos/branches/SAMBA_4_0/source/lib/tdb ldb-$version/libtdb
-else
-	# Release
-	svn export $samba_repos/tags/LDB_`echo $version | sed 's/\./_/g'` ldb-$version
+if [ -z "$GIT_URL" ]; then
+	echo "Usage: $0 <samba4-git-url> [<refspec>]"
+	exit 1
 fi
 
-cd ldb-$version && ./autogen.sh && cd ..
-tar cvz ldb-$version > ldb_$version.orig.tar.gz
+LDBTMP=$TMPDIR/$RANDOM.ldb.git
+version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
+git clone --depth 1 -l $GIT_URL $LDBTMP
+if [ ! -z "$REFSPEC" ]; then
+	pushd $LDBTMP
+	git checkout $REFSPEC
+	popd
+fi
+
+mv $LDBTMP/source/lib/ldb "ldb-$version"
+mv $LDBTMP/source/lib/replace "ldb-$version/libreplace"
+mv $LDBTMP/source/lib/tdb "ldb-$version/libtdb"
+mv $LDBTMP/source/lib/talloc "ldb-$version/libtalloc"
+rm -rf $LDBTMP
+pushd "ldb-$version" && ./autogen.sh && popd
+tar cvz "ldb-$version" > "ldb_$version.orig.tar.gz"
+rm -rf "ldb-$version"
