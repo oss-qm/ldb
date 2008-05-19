173276e4b716d0d5b5f113e20c5e5f8e5a10ed18
ctrlsoft-guest <ctrlsoft-guest>
2008-05-19 23:33:57 +0000

    Default to samba4 ldb, fix directory names.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -7,6 +7,10 @@ if [ -z "$GIT_URL" ]; then
 	GIT_URL=git://git.samba.org/samba.git
 fi
 
+if [ -z "$REFSPEC" ]; then
+	REFSPEC=origin/v4-0-test
+fi
+
 LDBTMP=$TMPDIR/$RANDOM.ldb.git
 version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
 git clone --depth 1 -l $GIT_URL $LDBTMP
@@ -18,8 +22,8 @@ fi
 
 mv $LDBTMP/source/lib/ldb "ldb-$version"
 mv $LDBTMP/source/lib/replace "ldb-$version/libreplace"
-mv $LDBTMP/source/lib/tdb "ldb-$version/libtdb"
-mv $LDBTMP/source/lib/talloc "ldb-$version/libtalloc"
+mv $LDBTMP/source/lib/tdb "ldb-$version/tdb"
+mv $LDBTMP/source/lib/talloc "ldb-$version/talloc"
 rm -rf $LDBTMP
 pushd "ldb-$version" && ./autogen.sh && popd
 tar cvz "ldb-$version" > "ldb_$version.orig.tar.gz"
