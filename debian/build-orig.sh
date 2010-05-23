674251882be909be496fb9ad4e6d5d7ace4d52e3
Jelmer Vernooij <jelmer@samba.org>
2010-05-23 15:55:22 +0200

    Update for waf build.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -1,31 +1,24 @@
 #!/bin/bash
-REFSPEC=$1
-GIT_URL=$2
+GIT_URL=$1
+REFSPEC=$2
 shift 2
 
 if [ -z "$GIT_URL" ]; then
 	GIT_URL=git://git.samba.org/samba.git
 fi
 
-if [ -z "$REFSPEC" ]; then
-	REFSPEC=origin/master
-fi
-
 LDBTMP=$TMPDIR/$RANDOM.ldb.git
-version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
-git clone --depth 1 -l $GIT_URL $LDBTMP
+git clone --depth 1 $GIT_URL $LDBTMP
 if [ ! -z "$REFSPEC" ]; then
 	pushd $LDBTMP
-	git checkout $REFSPEC
+	git checkout $REFSPEC || exit 1
 	popd
 fi
-
-mv $LDBTMP/source4/lib/ldb "ldb-$version"
-mv $LDBTMP/lib/replace "ldb-$version/libreplace"
-mv $LDBTMP/lib/tevent "ldb-$version/tevent"
-mv $LDBTMP/lib/tdb "ldb-$version/tdb"
-mv $LDBTMP/lib/talloc "ldb-$version/talloc"
+pushd $LDBTMP/lib/ldb
+./autogen-waf.sh
+./configure
+make dist
+popd
+version=$( dpkg-parsechangelog -l`dirname $0`/changelog | sed -n 's/^Version: \(.*:\|\)//p' | sed 's/-[0-9.]\+$//' )
+mv $LDBTMP/lib/ldb/ldb-*.tar.gz ldb-$version.tar.gz
 rm -rf $LDBTMP
-pushd "ldb-$version" && ./autogen.sh && popd
-tar cvz "ldb-$version" > "ldb_$version.orig.tar.gz"
-rm -rf "ldb-$version"
