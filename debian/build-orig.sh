2d77f3fa9187eb7b2ce81f158faa3082e8018a86
Jelmer Vernooij <jelmer@debian.org>
2010-09-28 09:27:12 +0200

    * New upstream snapshot. * New upstream snapshot.

--- debian/build-orig.sh
+++ debian/build-orig.sh
@@ -1,21 +1,17 @@
 #!/bin/bash
-GIT_URL=$1
-REFSPEC=$2
-shift 2
 
-if [ -z "$GIT_URL" ]; then
-	GIT_URL=git://git.samba.org/samba.git
+if [ -z "$SAMBA_GIT_URL" ]; then
+	SAMBA_GIT_URL=git://git.samba.org/samba.git
 fi
 
 LDBTMP=`mktemp -d`
-git clone --depth 1 $GIT_URL $LDBTMP
+git clone --depth 1 $SAMBA_GIT_URL $LDBTMP
 if [ ! -z "$REFSPEC" ]; then
 	pushd $LDBTMP
 	git checkout $REFSPEC || exit 1
 	popd
 fi
 pushd $LDBTMP/source4/lib/ldb
-./autogen-waf.sh
 ./configure
 make dist
 popd
